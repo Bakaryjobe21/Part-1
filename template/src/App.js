228e8d629a89df3994b5f26058121fa4959e50ce
Bakaryjobe21 <Bakaryjobe21@gmail.com>
2021-09-15 17:39:06 +0000

    1.3: course information step3

--- template/src/App.js
+++ template/src/App.js
@@ -1,78 +1,39 @@
 import React from 'react'
 
 const App = () => {
-  const course='Half stack application development'
-  const part1='Fundamentals of react'
-   const exercises1=10
-  const part2='Using props to pass data'
-  const exercises2=7
-  const part3='State of a component'
-  const exercises3=14
-  
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
-     <Header course={course}/>
-     <Content part1={part1} exercises1={exercises1} 
-              part2={part2} exercises2={exercises2}
-              part3={part3} exercises3={exercises3}/>
-    
-      <Total total={exercises1+exercises2+exercises3} />
-      
-     </div>
+       <h1>
+         {course}
+       </h1>
+       <p>
+         {part1.name} {part1.exercises}
+       </p>
+       <p>
+         {part2.name} {part2.exercises}
+       </p>
+       <p>
+         {part3.name} {part3.exercises}
+       </p>
 
-
-  )
-
-}
-
-const Header=(props)=>{
-
-  return(
-    <h1>
-      {props.course}
-    </h1>
-  )
-}
-  
-const Content =()=>{
-
-  return(
-    <div>
-      <Part part1={part1} exercises1={exercises1}/>
-      <Part part2={part2} exercises2={exercises2}/>
-      <Part  part3={part3} exercises3={exercises3}/>
-     
+       <p>
+         Total exercises {part1.exercises+part2.exercises+part3.exercises}
+       </p>
     </div>
-  )
-}
-
-
-const Part =(props)=>{
-
-  return(
-    <div>
-
-      <p> {props.part1}{props.exercises1}</p>
-     <p>
-       {props.part2} {props.exercises2}
-     </p>
-     <p>
-       {props.part3} {props.exercises3}
-     </p>
-    </div>
-  )
-}
-
-
-
-
-const Total =(props)=>{
-  return(
-  <div>
-    <p>
-     Total exercises {props.total}
-    </p>
-  </div>
   )
 }
 
