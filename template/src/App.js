9fa9ad6c4eb3bd06768bd24f8d8baa8e4e121446
Bakaryjobe21 <Bakaryjobe21@gmail.com>
2021-09-15 17:30:12 +0000

    1.2: course information, step2

--- template/src/App.js
+++ template/src/App.js
@@ -34,11 +34,25 @@ const Header=(props)=>{
   )
 }
   
-const Content =(props)=>{
+const Content =()=>{
 
   return(
     <div>
-     <p> {props.part1}{props.exercises1}</p>
+      <Part part1={part1} exercises1={exercises1}/>
+      <Part part2={part2} exercises2={exercises2}/>
+      <Part  part3={part3} exercises3={exercises3}/>
+     
+    </div>
+  )
+}
+
+
+const Part =(props)=>{
+
+  return(
+    <div>
+
+      <p> {props.part1}{props.exercises1}</p>
      <p>
        {props.part2} {props.exercises2}
      </p>
@@ -48,6 +62,8 @@ const Content =(props)=>{
     </div>
   )
 }
+
+
 
 
 const Total =(props)=>{
