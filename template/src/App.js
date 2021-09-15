b9945ed86ca7611c99f113dc71243a89147689d6
Bakaryjobe21 <Bakaryjobe21@gmail.com>
2021-09-15 17:59:07 +0000

    1.5: course information step5

--- template/src/App.js
+++ template/src/App.js
@@ -1,37 +1,43 @@
 import React from 'react'
 
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+
 
   return (
     <div>
        <h1>
-         {course}
+         {course.name}
        </h1>
        <p>
-         {part1.name} {part1.exercises}
+         {course.parts[0].name} {course.parts[0].exercises}
        </p>
        <p>
-         {part2.name} {part2.exercises}
+         {course.parts[1].name} {course.parts[1].exercises}
        </p>
        <p>
-         {part3.name} {part3.exercises}
+         {course.parts[2].name} {course.parts[2].exercises}
        </p>
 
        <p>
-         Total exercises {part1.exercises+part2.exercises+part3.exercises}
+         Total exercises {course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}
        </p>
     </div>
   )
