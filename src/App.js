1253bd6399edfb51d57b120f024771f429baa77d
Bakaryjobe21 <Bakaryjobe21@gmail.com>
2021-12-11 19:55:05 +0000

    1.12*: anecdotes step1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,42 @@ import  {React, useState} from "react"
 
  const App = () => {
 
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const buttonClicked=()=>{
+    setSelected(selected +1)
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]} <br/>
+
+      <Button buttonClicked={buttonClicked}  text='next anecdote'/>
+    </div>
+  )
+}
+const Button=({buttonClicked, text})=>{
+  return(
+    <button onClick={buttonClicked}>{text}</button>
+  )
+
+}
 
 
-  const [good, setGood] =useState(0)
+export default App
+
+
+
+ /* const [good, setGood] =useState(0)
   const [bad, setBad]=useState(0)
   const [neutral, setNeutral]=useState(0)
 
@@ -68,24 +101,12 @@ const StatisticLine=({good,neutral,bad,all,average,postive})=>{
     <div>
       <table> 
         <tbody>
-          <tr>
-            <td>good</td> <td>{good}</td>
-          </tr>
-          <tr>
-            <td>neutral</td> <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>bad</td> <td>{bad}</td>
-          </tr>
-          <tr>
-            <td>all</td> <td>{all}</td>
-          </tr>
-          <tr>
-            <td>average</td> <td>{average}</td>
-          </tr>
-          <tr>
-            <td>positive</td> <td>{postive}</td>
-          </tr>
+          <tr><td>good</td> <td>{good}</td> </tr>
+          <tr><td>neutral</td> <td>{neutral}</td></tr>
+          <tr><td>bad</td> <td>{bad}</td></tr>
+          <tr><td>all</td> <td>{all}</td></tr>
+          <tr><td>average</td> <td>{average}</td></tr>
+          <tr><td>positive</td> <td>{postive}</td></tr>
         </tbody> 
         
         </table>
@@ -95,16 +116,7 @@ const StatisticLine=({good,neutral,bad,all,average,postive})=>{
     </div>
   )
 }
-
-const Button=({buttonClicked, text})=>{
-  return(
-    <button onClick={buttonClicked}>{text}</button>
-  )
-
-}
-
-export default App
-
+*/
 
 
 
